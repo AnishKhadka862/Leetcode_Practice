676e08a67c687ec3f616a441a22dff62b38969c8
Anish Khadka <akhadka2@sfsu.edu>
2024-10-02 11:42:22 -0700

    Time: 4 ms (47.31%), Space: 8 MB (66.39%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int i = s.length() - 1;
+        int count = 0;
+
+        while (i >= 0 && isspace(s[i])) {
+            --i;
+        }
+
+        while (i >= 0 && !isspace(s[i])) {
+            count++;
+            i--;
+        }
+
+        return count;
+    }
+};
