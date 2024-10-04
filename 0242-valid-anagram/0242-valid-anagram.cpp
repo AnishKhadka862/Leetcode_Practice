5bb02625eea9d0cb524ff979db1cd75874079a74
Anish Khadka <akhadka2@sfsu.edu>
2024-10-04 07:57:47 -0700

    Time: 9 ms (51%), Space: 9.8 MB (39.7%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size()){
+            return false;
+        }
+        unordered_map < char, int> mymap;
+        int i = 0;
+        for (char c : s){
+            mymap[c]++;
+        }
+        for (char c : t){
+            if (mymap.find(c) != mymap.end()){
+                mymap[c]--;
+                if (mymap[c] == 0){
+                    mymap.erase(c);
+                }
+            }
+            else{
+                return false;
+            }
+        }
+        if (mymap.empty()){
+        return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
