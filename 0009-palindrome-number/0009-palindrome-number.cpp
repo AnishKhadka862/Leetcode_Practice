e2d930d82b539cba2611d87c663a0440778e4532
Anish Khadka <akhadka2@sfsu.edu>
2024-10-21 23:03:08 -0700

    Time: 4 ms (83.56%), Space: 8.5 MB (75.43%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,17 +1,37 @@
+// class Solution {
+// public:
+//     bool isPalindrome(int x) {
+//     // converting int to string
+//     string s = to_string(x);
+//     int left = 0;
+//     int right = s.size() - 1;
+//     while (left < right){
+//         if (s[left] != s[right]){
+//             return false;
+//         }
+//         left++;
+//         right--;
+//     }
+//     return true;
+//     }
+// };
+
+// without converting int to string
+
 class Solution {
 public:
     bool isPalindrome(int x) {
-    // converting int to string
-    string s = to_string(x);
-    int left = 0;
-    int right = s.size() - 1;
-    while (left < right){
-        if (s[left] != s[right]){
-            return false;
-        }
-        left++;
-        right--;
+
+    if (x < 0 || x % 10 == 0 && x != 0){
+        return false;
     }
-    return true;
+
+    int reversedHalf = 0;
+    while (x > reversedHalf){
+        reversedHalf = reversedHalf * 10 + (x % 10);
+        x /= 10;
+    }
+    return x == reversedHalf || x == reversedHalf / 10;
+
     }
 };
