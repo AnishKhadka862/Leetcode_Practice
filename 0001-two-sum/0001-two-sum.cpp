41b640115450ab39c51955468f8a211c5b97739f
Anish Khadka <akhadka2@sfsu.edu>
2025-10-17 11:29:14 -0700

    Time: 3 ms (69.34%), Space: 15 MB (19.86%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,15 +1,25 @@
+// [3, 5, 8, 6, 4] , target = 10
+// since, 3 and 4 adds up to 10
+// should return indices 3 and 4 -> [3, 4]
+//target = 10
+//mymap -> {3,0}
+//diff -> 7
+
+
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map <int, int> mymap;
         for (int i = 0; i < nums.size(); ++i){
-            int diff = target - nums[i];
+            int diff = target - nums[i]; // 7
             if (mymap.find(diff) != mymap.end()){
                 return {i, mymap[diff]};
             }
             else{
                 mymap[nums[i]] = i;
             }
+
         }
         return {};
     }
